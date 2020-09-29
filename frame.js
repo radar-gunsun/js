6177a27b2583cfd56192ee5e9da14671704f934f
jekson123 <59883852+jekson123@users.noreply.github.com>
2020-09-29 09:00:28 +0300

    init commit

---
+++ frame.js
@@ -0,0 +1,17 @@
+﻿    await navigateAsync('https://example.com/'); // Условно на данном сайте находится несколько фреймов.
+
+    // Последовательно извлекаем необходимый фрейм. 
+    // Сначала выбираем фрейм с индексом 0, затем вложенный в него фрейм, содержащий в URL текст ?query, затем вложенный в него фрейм с индексом 1
+    let frame = await selectFrameAsync([0, '?query', 1]); 
+
+
+    if(frame === null) {
+        throw 'фрейм не был найден.';
+    }
+    else {
+        // Выполняем следующие команды..
+    }
+
+    let activeTab=await commandAsync("get::activetab");//получить номер активной вкладки браузера
+
+    let frame = await selectFrameAsync([],activeTab); //получить главное окно активной вкладки
