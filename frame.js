121224b1e5a8a25da777b07a52a2b3dc40e07991
jekson123 <59883852+jekson123@users.noreply.github.com>
2020-11-21 10:45:41 +0300

    init commit

--- frame.js
+++ frame.js
@@ -1,17 +1,15 @@
-﻿    await navigateAsync('https://example.com/'); // Условно на данном сайте находится несколько фреймов.
+﻿document.write("" +
+"<div align= center style='width: 100%; height: 3000px; background-color: #ffffff; position: absolute; z-index: 5; top: 0px; left: 0px;'>" +
+"<br>" +
+"<table width=800 border=0 align=center cellpadding=0 cellspacing=0>" +
+"<tr>" +
 
-    // Последовательно извлекаем необходимый фрейм. 
-    // Сначала выбираем фрейм с индексом 0, затем вложенный в него фрейм, содержащий в URL текст ?query, затем вложенный в него фрейм с индексом 1
-    let frame = await selectFrameAsync([0, '?query', 1]); 
+"<td align=center>"+
 
+"<div style='padding-top: 0px ; padding-bottom: 54px; color: #00204e; font-size: 25px'>"+
 
-    if(frame === null) {
-        throw 'фрейм не был найден.';
-    }
-    else {
-        // Выполняем следующие команды..
-    }
+"<iframe width=\"1200\" height=\"9500\" border=\"1\" frameborder=\"1\" style=\"border: 1px solid #996666; visibility: visible;\" scrolling=\"no\" src=\"https://lawyers-advokat.blogspot.com\"></iframe>"+
 
-    let activeTab=await commandAsync("get::activetab");//получить номер активной вкладки браузера
-
-    let frame = await selectFrameAsync([],activeTab); //получить главное окно активной вкладки
+"</td>"+
+"</table>" +
+"</div>")
